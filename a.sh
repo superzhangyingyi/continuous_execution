6d102c9ae42dacc91fd216c98754a7b53a2d37a9
zyy <143052140+superzhangyingyi@users.noreply.github.com>
2023-08-25 14:27:21 +0800

    v2.1 前置条件
    开始时检测a状态：
    完成/空闲：可以直接运行b任务
    调度中或运行中：一秒检查一次a状态，当a跑完就会执行b

--- a.sh
+++ a.sh
@@ -1,3 +1,37 @@
-APIKEY="a360f1fc1e7cc76bc2d9c3156a40b599"
-JOBUUID="47c07aec3d8611ee800000505692225b"
-curl -H "X-API-Key:$APIKEY" -H "Content-Type: application/json" -X POST -d '[{"job_uuid": "'$JOBUUID'", "action":"start"}]' "http://192.168.3.118/d2/r/v2/job/action"
+COOKIE="dbackup3.session=05b02f04ffe9ea5e55c052aa6e2edf9efb72cb51204b5a63a8ebd69d32ca8759"
+TOKEN="ea36e2ab37a8d95773924313829da1e1"
+JOBUUID="52f1065c3b1a11ee800000505692225b"
+string=$(curl --cookie "$COOKIE" -H "Content-Type:application/json" -H "X-CSRF-Token:$TOKEN" "http://192.168.3.118/d2/r/v2/jobs?uuid=$JOBUUID")
+tp1=${string#*state}
+tp2=${tp1:3}
+aStatus=$(echo ${tp2} | awk -F '"' '{print $1}')
+case $aStatus in
+	"completed")
+		echo "canrun b"
+		;;
+	"running" | "starting")
+		echo "running or starting"
+		while true; do
+			string=$(curl --cookie "$COOKIE" -H "Content-Type:application/json" -H "X-CSRF-Token:$TOKEN" "http://192.168.3.118/d2/r/v2/jobs?uuid=$JOBUUID")
+			tp1=${string#*state}
+			tp2=${tp1:3}
+			aStatus2=$(echo ${tp2} | awk -F '"' '{print $1}')
+			if [ $aStatus2 == "running" ] || [ $aStatus2 == "starting" ]; then
+				echo "wait"
+			else
+				break
+			fi
+			sleep 1
+		done
+		if [ $aStatus2 == "completed" ] || [ $aStatus2 == "idle" ]; then
+			echo "can run b now"
+		else
+			echo "${aStatus2} cannot run"
+			exit 1
+		fi
+		;;
+	*)
+		echo "${aStatus} cannot run"
+		exit 1
+		;;
+esac
