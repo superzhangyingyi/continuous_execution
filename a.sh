c78dc4f583ab242b48a945ebc2ce781af0873a70
zyy <143052140+superzhangyingyi@users.noreply.github.com>
2023-08-25 14:04:42 +0800

    v1.2 api-key登录
    携带X-API-Key头完成身份验证

--- a.sh
+++ a.sh
@@ -1,4 +1,3 @@
-TOKEN="86eca62af2938be0b4568f8982ea947b"
-COOKIE="dbackup3.session=5b0413e737b0e2a4878de1fc2f6c68f0f74792fdc67fa9113b681f5990806b74"
+APIKEY="a360f1fc1e7cc76bc2d9c3156a40b599"
 JOBUUID="47c07aec3d8611ee800000505692225b"
-curl --cookie "$COOKIE" -H "X-CSRF-Token:$TOKEN" -H "Content-Type: application/json" -X POST -d '[{"job_uuid": "'$JOBUUID'", "action":"start"}]' "http://192.168.3.118/d2/r/v2/job/action"
+curl -H "X-API-Key:$APIKEY" -H "Content-Type: application/json" -X POST -d '[{"job_uuid": "'$JOBUUID'", "action":"start"}]' "http://192.168.3.118/d2/r/v2/job/action"
